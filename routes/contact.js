209d28996ffe4c95d208c388094a7ec6308cfc20
ArunKumar <arun.kum@peopleinteractive.in>
2025-10-01 16:09:40 +0530

    page setup

--- routes/contact.js
+++ routes/contact.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../utils/db");
+// const db = require("../utils/db");
 
-router.post("/", (req, res) => {
-  const { name, email, message } = req.body;
-  db.query(
-    "INSERT INTO messages (name, email, message) VALUES (?, ?, ?)",
-    [name, email, message],
-    (err, result) => {
-      if (err) return res.status(500).send("Database error");
-      res.send("Message saved successfully!");
-    }
-  );
-});
+// router.post("/", (req, res) => {
+//   const { name, email, message } = req.body;
+//   db.query(
+//     "INSERT INTO messages (name, email, message) VALUES (?, ?, ?)",
+//     [name, email, message],
+//     (err, result) => {
+//       if (err) return res.status(500).send("Database error");
+//       res.send("Message saved successfully!");
+//     }
+//   );
+// });
 
 module.exports = router;
